275c51e0d9b70fbae84360d2dceb0c29abdc9524
cieslinskice <cieslinskice@ldschurch.org>
2013-09-05 16:25:09 -0600

    Change language key to something more appropriate

--- src/main/java/org/sonar/plugins/xquery/api/XQueryConstants.java
+++ src/main/java/org/sonar/plugins/xquery/api/XQueryConstants.java
@@ -6,14 +6,14 @@ package org.sonar.plugins.xquery.api;
 
 
 public interface XQueryConstants {
-    static String XQUERY_LANGUAGE_KEY = "xqy";
+    static String XQUERY_LANGUAGE_KEY = "xquery";
     static String FILE_EXTENSIONS_KEY = "sonar.marklogic.fileExtensions";
     static String SOURCE_DIRECTORY_KEY = "sonar.marklogic.sourceDirectory";
     static String XQTEST_REPORTS_DIRECTORY_KEY = "sonar.xqtest.reportsPath";
 
     static String XQUERY_LANGUAGE_NAME = "XQuery";
-    static String[] DEFAULT_FILE_EXTENSIONS = { "xqy", "xquery", "xq" };
-    static String DEFAULT_FILE_EXTENSIONS_STRING = "xqy, xquery, xq";
+    static String[] DEFAULT_FILE_EXTENSIONS = { "xqy", "xquery", "xq" , "xqi", "xql", "xqm", "xqws"};
+    static String DEFAULT_FILE_EXTENSIONS_STRING = "xqy, xquery, xq, xqi, xql, xqm, xqws";
     static String DEFAULT_SOURCE_DIRECTORY = "src/main/xquery";
     static String DEFAULT_XQTEST_DIRECTORY = "target/xqtest-reports";
 }
