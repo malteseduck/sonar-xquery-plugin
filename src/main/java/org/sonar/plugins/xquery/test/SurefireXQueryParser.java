6b924062706e056831a0ef5aa77f5a6f8ac5ff15
Christopher Cieslinski <CieslinskiCE@ldschurch.org>
2014-07-17 14:45:46 -0600

    Updated licensing to indicate derived work

--- src/main/java/org/sonar/plugins/xquery/test/SurefireXQueryParser.java
+++ src/main/java/org/sonar/plugins/xquery/test/SurefireXQueryParser.java
@@ -1,5 +1,24 @@
 /*
- * © 2014 by Intellectual Reserve, Inc. All rights reserved.
+ * SonarQube Java
+ * Copyright (C) 2012 SonarSource
+ * dev@sonar.codehaus.org
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02
+ *
+ * Derived from SurefireJavaParser by:
+ *     Chris Cieslinski
  */
 
 package org.sonar.plugins.xquery.test;
